7066db24813365008f3ec440f4aace93f0015cf7
Salvador <sfandino@yahoo.com>
2011-05-25 18:45:16 +0200

    sample module

--- Sample.xs
+++ Sample.xs
@@ -4,6 +4,44 @@
 
 #include "ppport.h"
 
+/* define int64_t and uint64_t when using MS compiler */
+#ifdef _MSC_VER
+#include <stdlib.h>
+typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
+#endif
+
+/* uncomment the following line to use native IVs if they are 64bits long */
+/* #define MATH_INT64_NATIVE_IF_AVAILABLE */
+
+#include "perl_math_int64.h"
 
 MODULE = Math::Int64::C_API::Sample		PACKAGE = Math::Int64::C_API::Sample		
 
+BOOT:
+  MATH_INT64_BOOT;
+
+int64_t
+some_int64()
+CODE:
+    RETVAL = -42;
+OUTPUT:
+    RETVAL
+
+uint64_t
+uint64_sqr(u64)
+    uint64_t u64
+CODE:
+    RETVAL = u64 * u64;
+OUTPUT:
+    RETVAL
+
+int64_t
+int64_addmul(acu, mul1, mul2)
+    int64_t acu
+    int64_t mul1
+    int64_t mul2
+CODE:
+    RETVAL = acu + mul1 * mul2;
+OUTPUT:
+    RETVAL
--- lib/Math/Int64/C_API/Sample.pm
+++ lib/Math/Int64/C_API/Sample.pm
@@ -4,83 +4,48 @@ use 5.010001;
 use strict;
 use warnings;
 
-require Exporter;
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Math::Int64::C_API::Sample ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
+use Exporter qw(import);
+our @EXPORT_OK = qw(some_int64 uint64_sqr int64_addmul);
 
 our $VERSION = '0.01';
 
 require XSLoader;
 XSLoader::load('Math::Int64::C_API::Sample', $VERSION);
 
-# Preloaded methods go here.
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-Math::Int64::C_API::Sample - Perl extension for blah blah blah
+Math::Int64::C_API::Sample - C API Usage sample for Math::Int64
 
 =head1 SYNOPSIS
 
   use Math::Int64::C_API::Sample;
-  blah blah blah
 
 =head1 DESCRIPTION
 
-Stub documentation for Math::Int64::C_API::Sample, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
+Look at the source
 
 =head2 EXPORT
 
-None by default.
+The sample subrutines available from this module are:
 
-
+  some_int64()
+  uint64_sqr(u64)
+  int64_addmul(acu, mul1, mul2)
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+L<Math::Int64>
 
 =head1 AUTHOR
 
-Salvador Fandino, E<lt>salva@E<gt>
+Salvador FandiE<ntilde>o, E<lt>sfandino@yahoo.comE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2011 by Salvador Fandino
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.10.1 or,
-at your option, any later version of Perl 5 you may have available.
-
+The contents of this package are on the public domain.
 
 =cut
